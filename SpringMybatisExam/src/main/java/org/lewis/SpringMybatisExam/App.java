2a2ca7ce1aeb9101988a07d3e12c541a2ffc3e50
10960 <changjiang.li@hand-china.com>
2016-08-15 23:49:56 +0800

    again

--- SpringMybatisExam/src/main/java/org/lewis/SpringMybatisExam/App.java
+++ SpringMybatisExam/src/main/java/org/lewis/SpringMybatisExam/App.java
@@ -1,8 +1,14 @@
 package org.lewis.SpringMybatisExam;
 
 
+import java.util.Date;
+import java.util.Scanner;
+
+
 import org.lewis.SpringMybatisExam.Entity.Country;
+import org.lewis.SpringMybatisExam.Entity.Customer;
 import org.lewis.SpringMybatisExam.Mapper.CountryMapper;
+import org.lewis.SpringMybatisExam.Service.CustomerServiceImp;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
@@ -14,13 +20,31 @@ public class App
 	
     public static void main( String[] args )
     {
-    	ApplicationContext ctx= new ClassPathXmlApplicationContext("ApplicationContext.xml");
-    	
-    	Country c=(Country) ctx.getBean("country");
-    	
-    	CountryMapper countryMapper=(CountryMapper) ctx.getBean("countryMapper");
-    	
-    	Country user = countryMapper.findById(1);
-    	System.out.println(user);
+    	 Scanner scanner= new Scanner(System.in);
+         System.out.print("请输入 FirstName(first_name):");
+         String firstName=scanner.next();
+         System.out.print("请输入 LastName(last_name):");
+         String lastName=scanner.next();
+         System.out.print("请输入 Email(email)::");
+         String email=scanner.next();
+         System.out.print("请输入  Address ID:");
+         Short addressId= scanner.nextShort();
+         
+         
+         Byte shortId=1;
+         
+         Customer customer= new Customer();
+         Date date=new Date();
+         customer.setAddressId(addressId);
+         customer.setFirstName(firstName);
+         customer.setLastName(lastName);
+         customer.setEmail(email);
+         customer.setStoreId(shortId);
+         customer.setCreateDate(date);
+         
+         CustomerServiceImp csi=new CustomerServiceImp();
+         
+         int insert= csi.insert(customer);
+         System.out.println(insert);
     }
 }
--- SpringMybatisExam/src/main/java/org/lewis/SpringMybatisExam/Service/CustomerService.java
+++ SpringMybatisExam/src/main/java/org/lewis/SpringMybatisExam/Service/CustomerService.java
@@ -1,43 +1,7 @@
 package org.lewis.SpringMybatisExam.Service;
 
-import java.util.List;
-
 import org.lewis.SpringMybatisExam.Entity.Customer;
-import org.lewis.SpringMybatisExam.Mapper.CustomerMapper;
-import org.mybatis.spring.SqlSessionTemplate;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Repository;
 
-
-@Repository 
-public class CustomerService implements CustomerMapper {
-
-	
-	 @Autowired  
-	    private SqlSessionTemplate sqlSessionTemplate;  
-	 
-	public void save(Customer customer) {
-		 sqlSessionTemplate.insert("insert-user", customer);  
-
-	}
-
-	public void update(Customer customer) {
-		 sqlSessionTemplate.update("update-user", customer);  
-
-	}
-
-	public void delete(int id) {
-		 sqlSessionTemplate.delete("delete-user",id);  
-
-	}
-
-	public Customer findById(int id) {
-		 return (Customer) sqlSessionTemplate.selectOne("queryById", 3);  
-	}
-
-	public List<Customer> findAll() {
-		 List<Customer> customers = sqlSessionTemplate.selectList("queryAll");  
-	        return customers;  
-	}
-
+public interface CustomerService {
+      public int  insert(Customer customer);
 }
